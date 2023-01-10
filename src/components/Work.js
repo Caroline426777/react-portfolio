0692ccf9aef93389e4b419ceae1743271bce26dc
426777 <426777@student.fontys.nl>
2023-01-10 11:18:52 +0100

    using props for work cards

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,13 +1,51 @@
-import './Work.css';
-import WorkCard from './WorkCard';
+import "./Work.css";
+import WorkCard from "./WorkCard";
 
-function Work(){
-    return(
-        <div className="work">
-            <h1 className="work__head font-link">Work</h1>
-            <WorkCard></WorkCard>
-        </div>
-    )
+import dafimg from "../assets/MockupDAF2.png";
+import strijpimg from "../assets/MockupStrijp.png";
+import ladiesimg from "../assets/MockupLadiesGamers.png";
+import ddwimg from "../assets/MockupDDW.png";
+import ellieimg from "../assets/MockupEllie2.png";
+import lickaliximg from "../assets/MockupLickalix.png";
+
+function Work() {
+  return (
+    <div className="work">
+      <h1 className="work__head font-link">Work</h1>
+      <div className="work__cards">
+        <WorkCard
+          img={dafimg}
+          title="DAF Dashboard Tool"
+          description="A WebApp where the user can learn how to truck dashboard and switched work in different ways."
+        />
+        <WorkCard
+          img={strijpimg}
+          title="Strijp-T Hotspot WebApp"
+          description="A Web Application with a map of Strijp-T with various hotspots. This way the user gets information about that location and a piece of history."
+        />
+        <WorkCard
+          img={ladiesimg}
+          title="LadiesGamers"
+          description="Banners and wallpapers for this gaming review website. They are a mixture between feminine and gaming."
+        />
+        <WorkCard
+          img={ddwimg}
+          title="Dutch Design Week"
+          description="An immersive experience of the Fontys ICT InnovationLab. This experience shows visitors how the building perceives them."
+        />
+        <WorkCard
+          img={ellieimg}
+          title="Interactive Ellie App"
+          description="The concepting and design of a WebApp that creates the users personal agenda based on their learning needs and keeps track of their progress."
+        />
+        <WorkCard
+          img={lickaliximg}
+          title="Lickalix packaging"
+          description="New compostable packaging for their organic and allergy free ice lollies. And a new styling for their YouTube content."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Work;
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,14 +1,15 @@
-import daf from '../assets/MockupDAF2.png';
-import './WorkCard.css'
+import "./WorkCard.css";
 
-function WorkCard(){
-    return(
-        <div className='work-card'>
-            <img className='work-card__img' src={daf} alt="DAF" />
-            <div className='work-card__overlay font-face-cb'>
-            DAF Dashboard tool</div>
-        </div>
-    )
+function WorkCard(props) {
+  return (
+    <div className="work-card">
+      <img className="work-card__img" src={props.img} alt="workimage"/>
+      <div className="work-card__overlay">
+        <div className="work-card__head font-face-cb">{props.title}</div>
+        <div className="work-card__body font-face-cr">{props.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default WorkCard;
